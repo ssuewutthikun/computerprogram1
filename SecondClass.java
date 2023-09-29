2fd37253b8de9e8af2e7d75f8a2dfb2385e5b0eb
ssuewutthikun <31299899+ssuewutthikun@users.noreply.github.com>
2023-09-29 10:02:29 +0700

    Update SecondClass.java

--- SecondClass.java
+++ SecondClass.java
@@ -4,13 +4,21 @@ public class SecondClass {
     public int y; 
     private  int score;
     public static void main(String[] args) {
-        FristClass obj = new FristClass();
+        SecondClass obj = new SecondClass();
+        obj.input_x();
+        obj.input_y();
+        System.out.print("value_x:"+obj.x);
+        System.out.print("value_y:"+obj.y);
     }
     public void input_x(){
-        System.out.println("input_x:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("input_x:");
+        x=scanner.nextInt();
     }
     public void input_y(){
-        System.out.println("input_y:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("input_y:");
+        y=scanner.nextInt();
     }
     private void score_calcurate(){
         System.out.println("score_calcurate");
