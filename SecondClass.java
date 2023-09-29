68d586fca8db22ed973d2a6b532a4346eecec7a7
ssuewutthikun <31299899+ssuewutthikun@users.noreply.github.com>
2023-09-29 09:37:57 +0700

    Create SecondClass.java

---
+++ SecondClass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; 
+public class SecondClass {
+    public int x;
+    public int y; 
+    private  int score;
+    public static void main(String[] args) {
+        FristClass obj = new FristClass();
+    }
+    public void input_x(){
+        System.out.println("input_x:");
+    }
+    public void input_y(){
+        System.out.println("input_y:");
+    }
+    private void score_calcurate(){
+        System.out.println("score_calcurate");
+    }
+    private void condition(){
+        System.out.println("condition");
+    }
+    public void display(){
+        System.out.println("display");
+    }
+}
